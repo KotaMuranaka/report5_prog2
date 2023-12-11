0c0fdbb08508151c4a4a16ec87019913466e404a
Kota Muranaka <e235751@ie.u-ryukyu.ac.jp>
2023-12-11 17:20:59 +0900

    step2

--- Report5.java
+++ Report5.java
@@ -1,6 +1,11 @@
 public class Report5 {
     public static void main(String[] args){
         String str=null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }catch(java.lang.NullPointerException e){
+            System.out.println("java.lang.NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
